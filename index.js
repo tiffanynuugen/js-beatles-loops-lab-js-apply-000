abee52a1266a0ad5dbe5b66657462018b4e6a0a6
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-07-31 05:25:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function theBeatlesPlay(musicians, instruments) {
 }
 
 for (i = 0; i < musicians.length; i++) {
-  	array.push(musicians + ' plays ' + instruments);
+  	array.push(musicians() + 'plays' + instruments());
   }
   return array;
 }
