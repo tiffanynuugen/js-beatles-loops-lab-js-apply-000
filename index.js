072d8cc8772e94455ac56c44218c71b4a505e884
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-07-31 06:20:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
 function theBeatlesPlay(musicians, instruments) {
     var allMusicians = [];
     for (var i = 0; i < musicians.length; i++) {
-        allMusicians.push(musicians[i] + " plays " + instruments[i]);
+        allMusicians.push(musicians[i] + "plays" + instruments[i]);
     }
     return allMusicians;
 }
-
-function johnLennonFacts(facts) {
-    var allFacts = [];
-    var i = 0;
-    while (i < facts.length) {
-      allFacts.push(facts[i] +"!!!");
-      i++;
-}
-    return allFacts;
