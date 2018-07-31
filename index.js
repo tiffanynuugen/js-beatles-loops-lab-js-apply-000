4c4492bbac6ffa94d67a47ee1604f62ec466d73e
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-07-31 05:51:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,16 +7,13 @@ function theBeatlesPlay(musicians, instruments) {
 }
 
 function johnLennonFacts() {
-  var facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
+    var facts = ["He was the last Beatle to learn to drive", 
+    "He was never a vegetarian", "He was a choir boy and boy scout", 
+    "He hated the sound of his own voice"];
     var newFacts = [];
     var i = 0;
     while (i < facts.length) {
-      newFacts.push(facts[i] + "!!!");
-      i++;
+        newFacts.push(facts[i] +"!!!");
+        i++;
     }
     return newFacts;
