c359a806fb4c8c2307647f9a1a17a5fe38279735
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-07-31 04:42:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// add solution here
+function theBeatlesPlay() {
+  var x = [];
+  const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+  const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+}
