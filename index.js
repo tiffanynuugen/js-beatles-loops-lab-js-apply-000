37e4db4a5de87150c3a1b0973c9a0c4687ecaf00
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-07-31 05:46:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ function theBeatlesPlay(musicians, instruments) {
 }
 
 function johnLennonFacts() {
+  const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
     var facts = [];
     var i = 0;
     while (i < facts.length) {
